d9611326e2d99e433f564cc84bab9cb4d695410f
Mikhail Chemodanov <mishachkld@gmail.com>
2026-01-24 19:35:32 +0200

    add lab1 on inverse problem

---
+++ Pestov/Lab1/Lab1.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.io import imread
+from skimage.color import rgb2gray
+from skimage import img_as_float
+from skimage.transform import radon, iradon
